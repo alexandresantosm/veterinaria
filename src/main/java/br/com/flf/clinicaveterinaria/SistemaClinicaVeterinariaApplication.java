57bd4e15305996ea3c48337b379741c0a5492739
alexandresantosm <amenezes031@gmail.com>
2020-06-02 17:47:18 -0300

    Criação de objetos do tipo espécie e inserção no banco de dados.

--- src/main/java/br/com/flf/clinicaveterinaria/SistemaClinicaVeterinariaApplication.java
+++ src/main/java/br/com/flf/clinicaveterinaria/SistemaClinicaVeterinariaApplication.java
@@ -1,15 +1,33 @@
 package br.com.flf.clinicaveterinaria;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.flf.clinicaveterinaria.domain.Especie;
+import br.com.flf.clinicaveterinaria.repository.EspecieRepository;
+
 @SpringBootApplication
-public class SistemaClinicaVeterinariaApplication {
+public class SistemaClinicaVeterinariaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaClinicaVeterinariaApplication.class, args);
 		
 		System.out.println("Hello World com Spring Boot!");
+	}
+
+	@Autowired
+	EspecieRepository especieRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Especie esp1 = new Especie(null, "Felino");
+		Especie esp2 = new Especie(null, "Canis");
+		
+		especieRepository.saveAll(Arrays.asList(esp1,esp2));
 	}
 
 }
