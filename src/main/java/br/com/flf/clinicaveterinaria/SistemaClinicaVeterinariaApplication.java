8dd38a18734bb789e54b8d21ea59069e43e3f5fe
alexandresantosm <amenezes031@gmail.com>
2020-06-02 18:20:06 -0300

    Criação de objetos do tipo veterinário e inserção no banco de dados.

--- src/main/java/br/com/flf/clinicaveterinaria/SistemaClinicaVeterinariaApplication.java
+++ src/main/java/br/com/flf/clinicaveterinaria/SistemaClinicaVeterinariaApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.flf.clinicaveterinaria.domain.Especie;
+import br.com.flf.clinicaveterinaria.domain.Veterinario;
 import br.com.flf.clinicaveterinaria.repository.EspecieRepository;
+import br.com.flf.clinicaveterinaria.repository.VeterinarioRepository;
 
 @SpringBootApplication
 public class SistemaClinicaVeterinariaApplication implements CommandLineRunner {
@@ -22,12 +24,21 @@ public class SistemaClinicaVeterinariaApplication implements CommandLineRunner {
 	@Autowired
 	EspecieRepository especieRepository;
 	
+	@Autowired
+	VeterinarioRepository veterinarioRepository; 
+	
 	@Override
 	public void run(String... args) throws Exception {
 		Especie esp1 = new Especie(null, "Felino");
 		Especie esp2 = new Especie(null, "Canis");
 		
 		especieRepository.saveAll(Arrays.asList(esp1,esp2));
+		
+		Veterinario v1 = new Veterinario(null, "Flavio Pinto", "12365654-45");
+		Veterinario v2 = new Veterinario(null, "Michel Temer", "9876543-33");
+		Veterinario v3 = new Veterinario(null, "Juciel Filho", "3434999-21");
+		
+		veterinarioRepository.saveAll(Arrays.asList(v1,v2,v3));
 	}
 
 }
