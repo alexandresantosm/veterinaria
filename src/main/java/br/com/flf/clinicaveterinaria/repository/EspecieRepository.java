d98240e1dec978f51b7192f58fde38e06d3fa3d6
alexandresantosm <amenezes031@gmail.com>
2020-06-02 17:26:18 -0300

    Adição de anotação na classe de repositório de espécies.

--- src/main/java/br/com/flf/clinicaveterinaria/repository/EspecieRepository.java
+++ src/main/java/br/com/flf/clinicaveterinaria/repository/EspecieRepository.java
@@ -1,9 +1,11 @@
 package br.com.flf.clinicaveterinaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.flf.clinicaveterinaria.domain.Especie;
 
+@Repository
 public interface EspecieRepository extends JpaRepository<Especie, Integer>{
 
 }
