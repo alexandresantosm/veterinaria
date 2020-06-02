aa8206091d8f70044b6a228b4ae1a863f14651b0
alexandresantosm <amenezes031@gmail.com>
2020-06-02 15:50:51 -0300

    Inicialização do Sistema de Clínica Veterinária.

---
+++ src/main/java/br/com/flf/clinicaveterinaria/SistemaClinicaVeterinariaApplication.java
@@ -0,0 +1,15 @@
+package br.com.flf.clinicaveterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClinicaVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClinicaVeterinariaApplication.class, args);
+		
+		System.out.println("Hello World com Spring Boot!");
+	}
+
+}
